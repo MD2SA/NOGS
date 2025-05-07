b19063da4256285cc761e7892f4f37de3533d70f
MD2SA <albuquerque2158@gmail.com>
2025-05-07 23:17:44 +0100

    cena do slq resolvida

--- nogs/Team/urls.py
+++ nogs/Team/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'team'
 
 urlpatterns = [
+    path('teams/', views.team_list, name='team-list'),
+    path('teams/create/', views.create_team, name='create-team'),
+    path('teams/<int:team_id>/join/', views.join_team, name='join-team'),
+    path('teams/leave/', views.leave_team, name='leave-team'),
 ]
