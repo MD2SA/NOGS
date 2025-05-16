ff987da89f013edbd3288ae34ca87006e56a3091
MD2SA <albuquerque2158@gmail.com>
2025-05-16 11:17:54 +0100

    table css

--- nogs/nogs-frontend/src/components/atoms/Table.js
+++ nogs/nogs-frontend/src/components/atoms/Table.js
@@ -1,35 +1,52 @@
-import React from 'react';
+import React, { useMemo, useEffect, useState } from 'react';
 import "../../css/Table.css";
 
-export default function Table({ data, width = 400, height = 250 }) {
-    const headers = Array.from(new Set(data.flatMap((row) => Object.keys(row))));
+export default function Table({ data }) {
+    const headers = useMemo(() => {
+        return Array.from(new Set(data.flatMap(row => Object.keys(row))));
+    }, [data]);
+
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const onResize = () => setWidth(window.innerWidth);
+        window.addEventListener('resize', onResize);
+        return () => window.removeEventListener('resize', onResize);
+    }, []);
+
+    const estimatedWidth = headers.reduce((acc, header) => acc + (header.length + 3) * (width / 100), 0);
 
     const renderHeader = () => {
         return (
             <div className="table-header">
                 {headers.map((header) => (
-                    <span key={header} className="table-header-element">{header.toUpperCase()}</span>
+                    <span key={header} className="table-header-element">
+                        {header.toUpperCase()}
+                    </span>
                 ))}
             </div>
         );
-    }
+    };
 
     const renderRow = (row, rowIndex) => {
         return (
             <div className="table-row" key={rowIndex}>
                 {headers.map((header, columnIndex) => (
-                    <span key={rowIndex + "-" + columnIndex} className="table-row-element">{row[header] || "-"}</span>
+                    <span key={`${rowIndex}-${columnIndex}`} className="table-row-element">
+                        {row[header] || "-"}
+                    </span>
                 ))}
             </div>
         );
-    }
+    };
 
     return (
-        <div className="table-container" style={{ width: width }}>
+        <div className="table-container" style={{ width: `${estimatedWidth}px` }}>
             {renderHeader()}
-            <div className="table-body" style={{ height: height }}>
+            <div className="table-body" style={{ height: 250 }}>
                 {data.map((row, index) => renderRow(row, index))}
             </div>
         </div>
     );
 }
+
